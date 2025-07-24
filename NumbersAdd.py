3bd984b18a8f167d155ad4cd0e9bf3adcf1e0d40
mallik2020 <65417975+mallik2020@users.noreply.github.com>
2025-07-23 22:28:29 -0700

    test commit

--- NumbersAdd.py
+++ NumbersAdd.py
@@ -1,17 +1,1 @@
-def greet(name):
-  """Greets the person passed in as a parameter."""
-  return f"Hello, {name}!"
-
-def add(x, y):
-  """Add two numbers and return the result."""
-  return x + y
-
-def is_even(number):
-    """Check if a number is even."""
-    return number % 2 == 0
-
-if __name__ == "__main__":
-  print(greet("World"))
-  print(add(5, 3))
-  print(is_even(4))
-  print(is_even(7))
+Print("Nintendo")
